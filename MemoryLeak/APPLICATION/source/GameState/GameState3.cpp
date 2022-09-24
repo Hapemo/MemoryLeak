85e2f43ecb5655cb7716320cbc7847ac4e0e9671
Hapemo <jazz.teohyj@outlook.com>
2022-09-24 19:04:25 +0800

    Added gamestate 3 and editor mode control

---
+++ MemoryLeak/APPLICATION/source/GameState/GameState3.cpp
@@ -0,0 +1,31 @@
+#include "GameState3.h"
+
+
+void GameState3::Load() {
+
+}
+
+void GameState3::Init() {
+
+}
+
+void GameState3::Update() {
+
+}
+
+void GameState3::Draw() {
+
+}
+
+void GameState3::Free() {
+
+}
+
+void GameState3::Unload() {
+
+}
+
+
+
+
+
