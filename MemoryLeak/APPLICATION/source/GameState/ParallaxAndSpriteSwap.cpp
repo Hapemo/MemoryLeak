94c05d174dea45c8541b5a6a6743ec1c9361addc
kewyj <74573283+kewyj@users.noreply.github.com>
2022-09-27 20:09:53 +0800

    ,

--- MemoryLeak/APPLICATION/source/GameState/ParallaxAndSpriteSwap.cpp
+++ MemoryLeak/APPLICATION/source/GameState/ParallaxAndSpriteSwap.cpp
@@ -14,6 +14,9 @@ Game state for testing physics
 
 void ParallaxAndSpriteSwap::Load() {
     LOAD_TEXTURES("Background");
+    LOAD_TEXTURES("Icons");
+    LOAD_TEXTURES("Menu");
+    LOAD_TEXTURES("Sprites");
     LOAD_TEXTURES("Spritesheets");
     for (size_t index = 0; index < GET_RESOURCES().size(); ++index)
         spriteManager->InitializeTexture(GET_TEXTURE_DATA(index));
