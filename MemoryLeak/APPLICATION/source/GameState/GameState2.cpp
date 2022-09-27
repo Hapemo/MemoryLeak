13eef910a8702c28a2eb53e836e64e0c5c7382c1
kewyj <74573283+kewyj@users.noreply.github.com>
2022-09-27 21:04:11 +0800

    .

--- MemoryLeak/APPLICATION/source/GameState/GameState2.cpp
+++ MemoryLeak/APPLICATION/source/GameState/GameState2.cpp
@@ -23,29 +23,29 @@ void GameState2::Load() {
 }
 
 void GameState2::Init() {
-    /*************************************2500 Objects Start************************************/
-    serializationManager->LoadScene("SceneJGraphics");
-    // int i = 4000;
-    // int width = Application::getWindowWidth() >> 1;
-    // int height = Application::getWindowHeight() >> 1;
-    // while (i--)
-    // {
-    //     float tempx = (float)((-width + (std::rand() % (width * 2 + 1))));
-    //     float tempy = (float)((-height + (std::rand() % (height * 2 + 1))));
+    /*************************************4000 Objects Start************************************/
+    //serializationManager->LoadScene("SceneJGraphics");
+     int i = 4000;
+     int width = Application::getWindowWidth() >> 1;
+     int height = Application::getWindowHeight() >> 1;
+     while (i--)
+     {
+         float tempx = (float)((-width + (std::rand() % (width * 2 + 1))));
+         float tempy = (float)((-height + (std::rand() % (height * 2 + 1))));
 
-    //     /*******************if random number is outside of screen, crash**************/
-    //     /*******************ensures that everything is rendered in the screen!!*******/
-    //     if (tempx > width || tempx < -width)
-    //         std::exit(EXIT_FAILURE);
-    //     if (tempy > height || tempy < -height)
-    //         std::exit(EXIT_FAILURE);
+         /*******************if random number is outside of screen, crash**************/
+         /*******************ensures that everything is rendered in the screen!!*******/
+         if (tempx > width || tempx < -width)
+             std::exit(EXIT_FAILURE);
+         if (tempy > height || tempy < -height)
+             std::exit(EXIT_FAILURE);
 
-    //     Entity e1{ ECS::CreateEntity() };
-    //     e1.AddComponent(Transform{ {32,32}, 0, {tempx,tempy}},
-    //         Sprite{ Color{0,255,0,255}, SPRITE::TEXTURE, 0, 10 },
-    //         General{ "count" + i, TAG::PASSENGER, SUBTAG::NOSUBTAG, true });
-    //     spriteManager->SetTexture(e1, "Textures\\Sprites\\jumppad1.png");
-    // }
+         Entity e1{ ECS::CreateEntity() };
+         e1.AddComponent(Transform{ {32,32}, 0, {tempx,tempy}},
+             Sprite{ Color{0,255,0,255}, SPRITE::TEXTURE, 0, 10 },
+             General{ "count" + i, TAG::PASSENGER, SUBTAG::NOSUBTAG, true });
+         spriteManager->SetTexture(e1, "Textures\\Sprites\\jumppad1.png");
+     }
     /*************************************2500 Objects End************************************/
 }
 
