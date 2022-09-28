945db4c1480a670e89a1f640275c31307a713325
kewyj <74573283+kewyj@users.noreply.github.com>
2022-09-28 11:45:29 +0800

    commenting

--- MemoryLeak/VI/include/ECS/ECS_components.h
+++ MemoryLeak/VI/include/ECS/ECS_components.h
@@ -28,6 +28,10 @@ struct Lifespan {
 	//Lifespan(float _lifetime, float _limit) : lifetime(_lifetime), limit(_limit) {}
 };
 
+/*!*****************************************************************************
+\brief
+This component encapsulates what is needed in a Transform Component.
+*******************************************************************************/
 struct Transform
 {
 	Math::Vec2		scale;
@@ -35,6 +39,13 @@ struct Transform
 	Math::Vec2		translation;
 };
 
+/*!*****************************************************************************
+\brief
+This component encapsulates what is needed in a Sprite Component. 
+The sprite variable describes the type of sprite that should be rendered.
+The texture variable contains the texture id of the texture to be rendered.
+The layer variable contains which layer the sprite is to be rendered in.
+*******************************************************************************/
 struct Sprite
 {
 	Color color;
@@ -43,6 +54,14 @@ struct Sprite
 	int layer = 0;
 };
 
+
+/*!*****************************************************************************
+\brief
+This component encapsulates what is needed in an Animation component. 
+The images variable stores the texture id of images in the animation.
+The currentImageIndex contains the index of the texture in images vector to be
+used.
+*******************************************************************************/
 struct Animation
 {
 	std::vector<GLuint> images;
@@ -51,6 +70,13 @@ struct Animation
 	int currentImageIndex;
 };
 
+/*!*****************************************************************************
+\brief
+This component encapsulates what is needed in a SheetAnimation. Which is 
+animated using spritesheets.
+The frameCount variable stores the number of frames in a sprite.
+The currFrameIndex stores the index of the frame to be used.
+*******************************************************************************/
 struct SheetAnimation
 {
 	short frameCount = 1;
