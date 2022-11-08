002213a6e3cca733677f07b661da560058ee5031
Huang Wei Jhin <wjhuang15@gmail.com>
2022-11-08 23:04:40 +0800

    asset browser clean up and polish

--- MemoryLeak/VI/source/Editor/AssetPanel.cpp
+++ MemoryLeak/VI/source/Editor/AssetPanel.cpp
@@ -10,6 +10,9 @@ This file contains function declarations for a AssetPanel that view recources
 *******************************************************************************/
 #include "AssetPanel.h"
 #include <ECSManager.h>
+#define scrollrate 5
+#define minSize 50
+#define maxSize 200
 /*!*****************************************************************************
 \brief
 	Initializes the Asset Panel editor
@@ -19,7 +22,18 @@ None.
 *******************************************************************************/
 void AssetPanel::Init()
 {
-
+	colNum = 3;
+	folderSize = { 100,100 };
+	rootPath = "..\\resources";
+	sfxIcon = (void*)(intptr_t)spriteManager->GetTextureID("Textures\\Icons\\sfxIcon.png");
+	bgmIcon = (void*)(intptr_t)spriteManager->GetTextureID("Textures\\Icons\\bgmIcon.png");
+	backIcon = (void*)(intptr_t)spriteManager->GetTextureID("Textures\\Icons\\backIcon.png");
+	textIcon = (void*)(intptr_t)spriteManager->GetTextureID("Textures\\Icons\\textIcon.png");
+	sceneIcon = (void*)(intptr_t)spriteManager->GetTextureID("Textures\\Icons\\sceneIcon.png");
+	scriptIcon = (void*)(intptr_t)spriteManager->GetTextureID("Textures\\Icons\\scriptIcon.png");
+	folderIcon = (void*)(intptr_t)spriteManager->GetTextureID("Textures\\Icons\\folderIcon.png");
+	dialogueIcon = (void*)(intptr_t)spriteManager->GetTextureID("Textures\\Icons\\dialogueIcon.png");
+	gamestateIcon = (void*)(intptr_t)spriteManager->GetTextureID("Textures\\Icons\\gamestateIcon.png");
 }
 /*!*****************************************************************************
 \brief
@@ -30,47 +44,147 @@ None.
 *******************************************************************************/
 void AssetPanel::Update()
 {
-	GLuint my_image_texture = spriteManager->GetTextureID("Textures\\Icons\\folderIcon.png");
-	GLuint my_image2_texture = 0;
-	std::string rootPath = "..\\resources";
-	ImVec2 buttonSize = { 100,100 };
+	//GLuint my_image2_texture = 0;
+	//std::string rootPath = "..\\resources";
+	//ImVec2 buttonSize = { 100,100 };
 	ImGui::Begin("Asset Manager");
 	ImGui::BeginTabBar("Assets ");
 	if (ImGui::BeginTabItem("Content Browser")) {
 		// switch to the newly selected tab
+		calculateSize();
 		static std::filesystem::path m_CurrentDirectory = std::filesystem::path(rootPath);
 		if (m_CurrentDirectory != std::filesystem::path(rootPath))
 		{
-			if (ImGui::Button("<- BACK", buttonSize))
+			if (ImGui::ImageButton(backIcon, { folderSize.x, folderSize.y/2.5f }, ImVec2(0, 1), ImVec2(1, 0)))
 			{
 				m_CurrentDirectory = m_CurrentDirectory.parent_path();
 			}
+		}
+		else
+		{
+			ImGui::NewLine();
+			ImGui::NewLine();
 		}
 		for (auto& directory : std::filesystem::directory_iterator(m_CurrentDirectory))
 		{
 			const auto& path = directory.path();
 			auto relativePath = std::filesystem::relative(path, rootPath);
 			std::string filename = relativePath.filename().string();
-			ImGui::Columns(3);
+			ImGui::Columns(colNum);
 
-			ImTextureID folderIcon = (void*)(intptr_t)my_image_texture;
 			if (directory.is_directory())
 			{
-				ImGui::ImageButton(folderIcon, buttonSize, ImVec2(0, 1), ImVec2(1, 0));
+				ImGui::ImageButton(folderIcon, folderSize, ImVec2(0, 1), ImVec2(1, 0));
+				if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
+				{
+					if (directory.is_directory())
+						m_CurrentDirectory /= path.filename();
+				}
 			}
 			else
 			{
 				texPath = directory.path().string();
 				texPath = texPath.substr(13);
+				texParent = directory.path().parent_path().string();
 				texExt = directory.path().extension().string();
-				texfilename = directory.path().stem().string();
+				texFilename = directory.path().stem().string();
 				if (texExt == ".meta")
 					continue;
-				my_image2_texture = spriteManager->GetTextureID(texPath);
+				if (texParent.find("\\Audio\\BGM") !=std::string::npos)
+				{
+					ImGui::ImageButton(bgmIcon, folderSize, ImVec2(0, 1), ImVec2(1, 0));
+					if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
+					{
+						LOG_INFO("Playing " + texFilename);
+						audioManager->PlayAnySound(texFilename, 16);
+					}
+					if (ImGui::BeginDragDropSource())
+					{
+						const wchar_t* itemPath = (wchar_t*)texFilename.c_str();
+						ImGui::SetDragDropPayload("AUDIO", itemPath, (wcslen(itemPath) + 1) * sizeof(wchar_t), ImGuiCond_Once);
+						ImGui::EndDragDropSource();
+					}
+				}
+				else if (texParent.find("\\Audio\\SFX") != std::string::npos)
+				{
+					ImGui::ImageButton(sfxIcon, folderSize, ImVec2(0, 1), ImVec2(1, 0));
+					if (ImGui::BeginDragDropSource())
+					{
+						const wchar_t* itemPath = (wchar_t*)texFilename.c_str();
+						ImGui::SetDragDropPayload("AUDIO", itemPath, (wcslen(itemPath) + 1) * sizeof(wchar_t), ImGuiCond_Once);
+						ImGui::EndDragDropSource();
+					}
+				}
+				else if (texParent.find("\\Dialogs") != std::string::npos)
+				{
+					ImGui::ImageButton(dialogueIcon, folderSize, ImVec2(0, 1), ImVec2(1, 0));
+					if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
+					{
+						serializationManager->LoadDialogs(texFilename);
+						SetPannelIsActive(E_PANELID::DIALOGUE, true);
+					}
+					if (ImGui::BeginDragDropSource())
+					{
+						const wchar_t* itemPath = (wchar_t*)texFilename.c_str();
+						ImGui::SetDragDropPayload("Dialoge", itemPath, (wcslen(itemPath) + 1) * sizeof(wchar_t), ImGuiCond_Once);
+						ImGui::EndDragDropSource();
+					}
+				}
+				else if (texParent.find("\\Fonts") != std::string::npos)
+				{
+					ImGui::ImageButton(textIcon, folderSize, ImVec2(0, 1), ImVec2(1, 0));
+					if (ImGui::BeginDragDropSource())
+					{
+						const wchar_t* itemPath = (wchar_t*)texFilename.c_str();
+						ImGui::SetDragDropPayload("FONT", itemPath, (wcslen(itemPath) + 1) * sizeof(wchar_t), ImGuiCond_Once);
+						ImGui::EndDragDropSource();
+					}
+				}
+				else if (texParent.find("\\GameStates") != std::string::npos)
+				{
+					ImGui::ImageButton(gamestateIcon, folderSize, ImVec2(0, 1), ImVec2(1, 0));
+				}
+				else if (texParent.find("\\Scene") != std::string::npos)
+				{
+					ImGui::ImageButton(sceneIcon, folderSize, ImVec2(0, 1), ImVec2(1, 0));
+					if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
+					{
+						ECS::DestroyAllEntities();
+						SceneReset();
+						serializationManager->LoadScene(texFilename);
+					}
+				}
+				else if (texParent.find("\\Scripts") != std::string::npos)
+				{
+					ImGui::ImageButton(scriptIcon, folderSize, ImVec2(0, 1), ImVec2(1, 0));
+					if (ImGui::BeginDragDropSource())
+					{
+						const wchar_t* itemPath = (wchar_t*)texFilename.c_str();
+						ImGui::SetDragDropPayload("SCRIPT", itemPath, (wcslen(itemPath) + 1) * sizeof(wchar_t), ImGuiCond_Once);
+						ImGui::EndDragDropSource();
+					}
+				}
+				else if (texParent.find("\\Textures") != std::string::npos)
+				{
+					selectedIcon = (void*)(intptr_t)spriteManager->GetTextureID(texPath);
+					ImGui::ImageButton(selectedIcon, folderSize, ImVec2(0, 1), ImVec2(1, 0));
+					if (texExt == ".png" || texExt == ".jpg")
+					{
+						if (ImGui::BeginDragDropSource())
+						{
+							const wchar_t* itemPath = (wchar_t*)texPath.c_str();
+							ImGui::SetDragDropPayload("TEXTURES", itemPath, (wcslen(itemPath) + 1) * sizeof(wchar_t), ImGuiCond_Once);
+							ImGui::EndDragDropSource();
+						}
+					}
+				}
+
+
+				/*my_image2_texture = spriteManager->GetTextureID(texPath);
 				if (my_image2_texture)
 				{
-					ImTextureID textureImage = (void*)(intptr_t)my_image2_texture;
-					ImGui::ImageButton(textureImage, buttonSize, ImVec2(0, 1), ImVec2(1, 0));
+					selectedIcon = (void*)(intptr_t)my_image2_texture;
+					ImGui::ImageButton(selectedIcon, buttonSize, ImVec2(0, 1), ImVec2(1, 0));
 					if (texExt == ".png" || texExt == ".jpg")
 					{
 						if (ImGui::BeginDragDropSource())
@@ -84,12 +198,15 @@ void AssetPanel::Update()
 				}
 				else
 				{
-					ImGui::Button(filename.c_str(), buttonSize);
+					if(ImGui::Button(filename.c_str(), buttonSize))
+					{
+						//std::cout << directory.path().parent_path() << "\n";
+					}
 					if (texExt == ".wav")
 					{
 						if (ImGui::BeginDragDropSource())
 						{
-							const wchar_t* itemPath = (wchar_t*)texfilename.c_str();
+							const wchar_t* itemPath = (wchar_t*)texFilename.c_str();
 							ImGui::SetDragDropPayload("AUDIO", itemPath, (wcslen(itemPath) + 1) * sizeof(wchar_t), ImGuiCond_Once);
 							ImGui::EndDragDropSource();
 						}
@@ -98,7 +215,7 @@ void AssetPanel::Update()
 					{
 						if (ImGui::BeginDragDropSource())
 						{
-							const wchar_t* itemPath = (wchar_t*)texfilename.c_str();
+							const wchar_t* itemPath = (wchar_t*)texFilename.c_str();
 							ImGui::SetDragDropPayload("SCRIPT", itemPath, (wcslen(itemPath) + 1) * sizeof(wchar_t), ImGuiCond_Once);
 							ImGui::EndDragDropSource();
 						}
@@ -107,14 +224,14 @@ void AssetPanel::Update()
 					{
 						if (ImGui::BeginDragDropSource())
 						{
-							const wchar_t* itemPath = (wchar_t*)texfilename.c_str();
+							const wchar_t* itemPath = (wchar_t*)texFilename.c_str();
 							ImGui::SetDragDropPayload("FONT", itemPath, (wcslen(itemPath) + 1) * sizeof(wchar_t), ImGuiCond_Once);
 							ImGui::EndDragDropSource();
 						}
 					}
-				}
+				}*/
 			}
-			if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
+			/*if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
 			{
 				if (directory.is_directory())
 					m_CurrentDirectory /= path.filename();
@@ -123,16 +240,16 @@ void AssetPanel::Update()
 					if (texExt == ".json")
 					{
 						
-						if (texfilename.substr(0, 6) == "Dialog")
+						if (texFilename.substr(0, 6) == "Dialog")
 						{
-							serializationManager->LoadDialogs(texfilename);
+							serializationManager->LoadDialogs(texFilename);
 							SetPannelIsActive(E_PANELID::DIALOGUE ,true);
 						}
-						else if (texfilename.substr(0, 5) == "Scene")
+						else if (texFilename.substr(0, 5) == "Scene")
 						{
 							ECS::DestroyAllEntities();
 							SceneReset();
-							serializationManager->LoadScene(texfilename);
+							serializationManager->LoadScene(texFilename);
 						}
 					}
 					else if (texExt == ".png")
@@ -141,22 +258,18 @@ void AssetPanel::Update()
 					else if (texExt == ".wav")
 					{
 						//audioManager->Play(texfilename);
-						std::cout << "play " << texfilename << "\n";
-						audioManager->PlayAnySound(texfilename, 16);
+						std::cout << "play " << texFilename << "\n";
+						audioManager->PlayAnySound(texFilename, 16);
 					}
-
 				}
-			}
-			ImGui::Text(filename.c_str());
+			}*/
+			ImGui::TextWrapped(filename.c_str());
 			ImGui::NextColumn();
 		}
 		ImGui::Columns(1);
 		ImGui::NewLine();
-		ImGui::NewLine();
 		ImGui::EndTabItem();
 	}
-	ImGui::Columns(1);
-	ImGui::NewLine();
 	ImGui::EndTabBar();
 	ImGui::End();
 }
@@ -170,4 +283,22 @@ None.
 void AssetPanel::Free()
 {
 
+}
+void AssetPanel::calculateSize()
+{
+	if (ImGui::IsWindowHovered() && (Input::CheckKey(E_STATE::HOLD, E_KEY::M_BUTTON_L)) || (Input::CheckKey(E_STATE::HOLD, E_KEY::LEFT_CONTROL)))
+	{
+		if (Input::GetScroll() > 0.0 && (folderSize.x < maxSize)) //scroll up   // zoon in
+		{
+			folderSize.x += scrollrate;
+			folderSize.y += scrollrate;
+		}
+		else if (Input::GetScroll() < 0.0 && (folderSize.x > minSize))  //scroll down //zoom out
+		{
+			folderSize.x -= scrollrate;
+			folderSize.y -= scrollrate;
+		}
+	}
+	colNum = (int)(ImGui::GetWindowWidth() / folderSize.x) - 1;
+	colNum = colNum > 0 ? colNum : 1;
 }
