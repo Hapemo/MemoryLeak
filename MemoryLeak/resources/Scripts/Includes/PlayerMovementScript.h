10d3782cf0816cbfa8bd95813f32ab85356c4c88
S13nd3rT3l3tub <40754433+S13nd3rT3l3tub@users.noreply.github.com>
2022-11-23 11:54:59 +0800

    Edited speed values

--- MemoryLeak/resources/Scripts/Includes/PlayerMovementScript.h
+++ MemoryLeak/resources/Scripts/Includes/PlayerMovementScript.h
@@ -7,7 +7,7 @@
 class PlayerMovementScript : public ScriptComponent {
 private:
 	std::string name{ "PlayerMovementScript" };
-	float playerSpeed{ 5.f };
+	float playerSpeed{ 10.f };
 public:
 	PlayerMovementScript() = default;
 	~PlayerMovementScript() override = default;
