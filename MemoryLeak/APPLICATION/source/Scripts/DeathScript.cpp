7680504c09fc8e48cf235607e102bad4dd5f66fe
jiawendh <jiawendh@gmail.com>
2022-11-25 15:24:53 +0800

    death script

---
+++ MemoryLeak/APPLICATION/source/Scripts/DeathScript.cpp
@@ -0,0 +1,48 @@
+/*!*****************************************************************************
+\file DeathScript.cpp
+\author Chen Jia Wen
+\par DP email: c.jiawen\@digipen.edu
+\par Course: GAM200
+\par Group: Memory Leak Studios
+\date 02-11-2022
+\brief
+This file contains the function definitions of the class DeathScript.
+*******************************************************************************/
+
+#include "DeathScript.h"
+
+REGISTER_SCRIPT(ScriptComponent, DeathScript);
+
+/*!*****************************************************************************
+\brief
+Function will run on initialisation of the entity.
+*******************************************************************************/
+void DeathScript::StartScript(Entity const& _e) {
+	(void)_e;
+	//LOG_INFO("Death script starts works!!!");
+	player = FUNC->GetEntity("Boat", "Level1");
+}
+
+/*!*****************************************************************************
+\brief
+Function will run on every update while the entity is active.
+*******************************************************************************/
+void DeathScript::UpdateScript(Entity const& _e) {
+	if (FUNC->EntitiesCollided(player, _e)) {
+		FUNC->SetTexture(player, "Textures\\Spritesheets\\MC\\mc-death-spritesheet.png");
+		player.GetComponent<SheetAnimation>().frameCount = 13;
+	}
+	else {
+		//FUNC->SetTexture(player, "Textures\\Spritesheets\\MC\\mc-death-spritesheet.png");
+		player.GetComponent<SheetAnimation>().frameCount = 8;
+	}
+}
+
+/*!*****************************************************************************
+\brief
+Function will run on exit or when the entity is destroyed.
+*******************************************************************************/
+void DeathScript::EndScript(Entity const& _e) {
+	(void)_e;
+	//LOG_INFO("Death script end works!!!");
+}
