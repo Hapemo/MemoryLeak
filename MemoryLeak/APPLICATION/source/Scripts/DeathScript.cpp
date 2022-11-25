d369188475ebbc6d4654b35aa2ad8044294cd00e
Huang Wei Jhin <wjhuang15@gmail.com>
2022-11-25 20:47:05 +0800

    yeeet

--- MemoryLeak/APPLICATION/source/Scripts/DeathScript.cpp
+++ MemoryLeak/APPLICATION/source/Scripts/DeathScript.cpp
@@ -28,13 +28,28 @@ void DeathScript::StartScript(Entity const& _e) {
 Function will run on every update while the entity is active.
 *******************************************************************************/
 void DeathScript::UpdateScript(Entity const& _e) {
+	static bool onEntry = false;
 	if (FUNC->EntitiesCollided(player, _e)) {
+		onEntry = true;
+	}
+	if (onEntry==true) //once collided
+	{
+		static bool first = false;
 		FUNC->SetTexture(player, "Textures\\Spritesheets\\BOAT\\capsize\\Props_Boat_NE_Capsize_Spritesheet.png");
-		player.GetComponent<SheetAnimation>().frameCount = 11;
+		if (first == false) //set stuff once
+		{
+			player.GetComponent<SheetAnimation>().frameCount = 11;
+			player.GetComponent<SheetAnimation>().timePerFrame = 0.15f;
+			player.GetComponent<SheetAnimation>().currFrameIndex = 0;
+			first = true;
+		}
+			
 		if (player.GetComponent<SheetAnimation>().currFrameIndex == player.GetComponent<SheetAnimation>().frameCount - 1) {
 			player.Deactivate();
 			(FUNC->SelectScene("Level1")).Pause(true);
 			(FUNC->SelectScene("Game Over")).Pause(false);
+			onEntry = false;
+			first = false;
 		}
 	}
 }
