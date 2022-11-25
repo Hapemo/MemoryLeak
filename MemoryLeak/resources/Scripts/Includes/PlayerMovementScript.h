cd932080e2a2037456f90de71986c4160ea39572
S13nd3rT3l3tub <40754433+S13nd3rT3l3tub@users.noreply.github.com>
2022-11-25 08:15:13 +0800

    Lowered player speed

--- MemoryLeak/resources/Scripts/Includes/PlayerMovementScript.h
+++ MemoryLeak/resources/Scripts/Includes/PlayerMovementScript.h
@@ -7,7 +7,7 @@
 class PlayerMovementScript : public ScriptComponent {
 private:
 	std::string name{ "PlayerMovementScript" };
-	float playerSpeed{ 20.f };
+	float playerSpeed{ 10.f };
 public:
 	PlayerMovementScript() = default;
 	~PlayerMovementScript() override = default;
