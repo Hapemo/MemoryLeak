f3d6bfa53a7ef68fa6911e9a2d76920e28beb424
S13nd3rT3l3tub <40754433+S13nd3rT3l3tub@users.noreply.github.com>
2022-11-27 00:38:25 +0800

    Removed unused variable in Contact

--- MemoryLeak/VI/include/Physics/Contact.h
+++ MemoryLeak/VI/include/Physics/Contact.h
@@ -95,7 +95,6 @@ public:
 	float penetration{};			// Collision penetration depths
 	float combinedRestitution{};	// Combined restitution value
 	float combinedFriction{};		// Combined friction value
-	Math::Vec2 newVelocity[2]{};	// Computed new velocity after collision
 	float seperatingVelocity{};		// Computed seperating velocity scalar value
 	float contactImpulse{};			// Contact impulse
 	Math::Vec2 contacts{};			// Points of contact of the collision
