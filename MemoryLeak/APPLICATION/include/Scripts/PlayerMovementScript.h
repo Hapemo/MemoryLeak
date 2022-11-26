c990cddd95f7eb04c6705505728e8e18bc0121ad
S13nd3rT3l3tub <40754433+S13nd3rT3l3tub@users.noreply.github.com>
2022-11-26 21:31:25 +0800

    Comments

--- MemoryLeak/APPLICATION/include/Scripts/PlayerMovementScript.h
+++ MemoryLeak/APPLICATION/include/Scripts/PlayerMovementScript.h
@@ -1,9 +1,25 @@
+/*!*****************************************************************************
+\file	PlayerMovementScript.h
+\author Lee Hsien Wei, Joachim
+\par	DP email: l.hsienweijoachim@digipen.edu
+\par	Course: GAM200
+\par	Group: Memory Leak Studios
+\date	26-11-2022
+\brief  This file contains the declaration of the PlayerMovementScript class
+		that defines the game logic used to move the player
+*******************************************************************************/
 #pragma once
 
+// -----------------------------
+// Include files
+// -----------------------------
 #include "ECS_tools.h"
 #include "ScriptManager.h"
 #include "ScriptComponent.h"
 
+/*!*****************************************************************************
+\brief PlayerMovementScript class that handles game logic for player movement
+*******************************************************************************/
 class PlayerMovementScript : public ScriptComponent {
 private:
 	std::string name{ "PlayerMovementScript" };
@@ -21,12 +37,45 @@ private:
 	Math::Vec2 initialCamScale;
 
 public:
+	/*!*****************************************************************************
+	\brief
+	Default constructor
+	*******************************************************************************/
 	PlayerMovementScript() = default;
+
+	/*!*****************************************************************************
+	\brief
+	Default destructor.
+	*******************************************************************************/
 	~PlayerMovementScript() override = default;
 
+	/*!*****************************************************************************
+	\brief
+	Function that contains logic to run on script start
+	\param const Entity &
+	A reference to read-only entity to update
+	\return void
+	NULL
+	*******************************************************************************/
 	void StartScript(const Entity& _e) override;
 
+	/*!*****************************************************************************
+	\brief
+	Function that contains logic to run during each update frame
+	\param const Entity &
+	A reference to read-only entity to update
+	\return void
+	NULL
+	*******************************************************************************/
 	void UpdateScript(const Entity& _e) override;
 
+	/*!*****************************************************************************
+	\brief
+	Function that contains logic to run on script end
+	\param const Entity &
+	A reference to read-only entity to update
+	\return void
+	NULL
+	*******************************************************************************/
 	void EndScript(const Entity& _e) override;
 };
