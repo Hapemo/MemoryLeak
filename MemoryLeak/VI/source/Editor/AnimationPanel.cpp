2119faf9be2a7b3ae06d504f3491d0596ac14aa1
Huang Wei Jhin <wjhuang15@gmail.com>
2023-01-13 13:09:21 +0800

    lines lol

--- MemoryLeak/VI/source/Editor/AnimationPanel.cpp
+++ MemoryLeak/VI/source/Editor/AnimationPanel.cpp
@@ -123,7 +123,11 @@ void AnimationPanel::Update()
 						else
 							imageSize = { ImGui::GetWindowWidth()-100.f, (ImGui::GetWindowWidth()-100.f) / ratio / e.GetComponent<Animation>().sheets[i].frameCount };
 						textureImage = (void*)(intptr_t)e.GetComponent<Animation>().sheets[i].sheet;
+						float sheetWidth = imageSize.x / e.GetComponent<Animation>().sheets[i].frameCount;
+						ImVec2 p = ImGui::GetCursorScreenPos();
 						ImGui::Image(textureImage, imageSize, ImVec2(0, 1), ImVec2(1, 0));
+						for(int w =1; w< e.GetComponent<Animation>().sheets[i].frameCount ; w++)
+							ImGui::GetWindowDrawList()->AddLine(ImVec2(p.x+ sheetWidth*w, p.y), ImVec2(p.x+ sheetWidth*w, p.y + imageSize.y), IM_COL32(30*w, 0, 30 * (8 - w), 255), 1.0f);
 						ImGui::SameLine();
 						ImGui::Text(("Sheet " + std::to_string(i)).c_str());
 					}
@@ -141,6 +145,7 @@ void AnimationPanel::Update()
 		{
 			ImGui::Text("Select an entity to edit");
 		}
+		
 	}
 	ImGui::End();
 }
