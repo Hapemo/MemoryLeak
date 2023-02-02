dd5540b74f30e5db9a29473e916f7c3cb675279a
Hapemo <jazz.teohyj@outlook.com>
2023-02-02 17:35:40 +0800

    pre final M4

--- MemoryLeak/SCRIPTING/MonoScript.cs
+++ MemoryLeak/SCRIPTING/MonoScript.cs
@@ -129,7 +129,7 @@ namespace BonVoyage
                   InternalCalls.UpdateText("objectivetext", "Dialogue", "Objective: Find the Little Girl");// for the start Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fermentum est nec rutrum venenatis. Suspendisse facilisis lectus ornare nisi feugiat, sed eleifend nisi molestie. Vestibulum et malesuada tortor. Donec eget diam vel lorem consequat tempus. Maecenas at mollis tellus. Maecenas dolor nisl, scelerisque a eleifend ornare, rhoncus nec leo. Praesent ultricies vehicula placerat. Etiam ligula enim, tempus sed tempor at, congue in ante. Ut condimentum non mauris ac efficitur."); // hint
                 }
               }
-              if (!RunIntroDialog) TextAlignChoices("objectivetext", "Dialogue", 400, 350, 530, 51.0f, 20, 10);
+              if (!RunIntroDialog) TextAlignChoices("objectivetext", "Dialogue", 500, 350, 530, 51.0f, 20, 10);
           }
     #endregion
 
@@ -287,14 +287,14 @@ namespace BonVoyage
               if (objectiveexpanded)
               {
                   InternalCalls.UpdateText("objectivetext", "Dialogue", "Objective: " + currentobjective); // hint
-                  TextAlignChoices("objectivetext", "Dialogue", 400, 350, 530, 51.0f, 20, 10);
+                  TextAlignChoices("objectivetext", "Dialogue", 500, 350, 530, 51.0f, 20, 10);
 
       }
 
       if (!objectiveexpanded)
               {
                   InternalCalls.UpdateText("objectivetext", "Dialogue", "Objective: Click to view"); // hint
-                  TextAlignChoices("objectivetext", "Dialogue", 400, 350, 530, 51.0f, 20, 10);
+                  TextAlignChoices("objectivetext", "Dialogue", 500, 350, 530, 51.0f, 20, 10);
 
       }
     }
@@ -507,11 +507,11 @@ namespace BonVoyage
         {
             currentobjective = InternalCalls.GetDialogue(InternalCalls.GetCurrentDialogueID());
             InternalCalls.UpdateText("objectivetext", "Dialogue", "Objective: " + currentobjective); // hint
-            TextAlignChoices("objectivetext", "Dialogue", 400, 350, 700, 51.0f, 20, 10);
+            TextAlignChoices("objectivetext", "Dialogue", 500, 350, 700, 51.0f, 20, 10);
             if (InternalCalls.GetLineCount("objectivetext", "Dialogue") > 2)
             {
                 InternalCalls.UpdateText("objectivetext", "Dialogue", "Objective: Click to view");
-                TextAlignChoices("objectivetext", "Dialogue", 400, 350, 530, 51.0f, 20, 10);
+                TextAlignChoices("objectivetext", "Dialogue", 500, 350, 530, 51.0f, 20, 10);
             }
         }
 
@@ -600,7 +600,8 @@ namespace BonVoyage
             InternalCalls.SetPosY(entityname, scenename, finalPosY);
 
             float textY = perLineScaleY * additionalLines / 2 - perLineScaleY/2;
-            InternalCalls.SetTextOffset(entityname, scenename, -scaleX / 2 + textXSpacing, textY);
+            float magicNumber = 10.0f * InternalCalls.GetFontScale(entityname, scenename);
+            InternalCalls.SetTextOffset(entityname, scenename, -scaleX / 2 + textXSpacing, textY + magicNumber);
         }
 
         #endregion
