793f7242a577523d7bebb74e0135eafbd734dca2
S13nd3rT3l3tub <40754433+S13nd3rT3l3tub@users.noreply.github.com>
2023-02-22 17:56:06 +0800

    Revert "Attempt at shifting playermovementscript to C#"
    This reverts commit 0b5852111f85b7f68f6c9093a39e685ce39dfd3a.

---
+++ MemoryLeak/resources/SCRIPTING/InternalCalls.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace BonVoyage
+{
+    public class InternalCalls
+	{
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static bool TestArgString(string _thingToPrint);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static string TestReturnString();
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static bool CheckKeyPress(int _key);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static bool CheckKeyHold(int _key);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static bool CheckKeyRelease(int _key);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static bool CheckKeyIdle(int _key);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static float GetWorldMousePosX();
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static float GetWorldMousePosY();
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void ApplyImpulse(string _entityName, string _sceneName, float _impulseX, float _impulseY, float _rotationX, float _rotationY);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void LoadDialogs(string _filename);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static string GetDialogue(int _id);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static int GetNextDialogueID(int _id);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static bool HasChoice(int _id);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static int GetChoice1(int _id);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static int GetChoice2(int _id);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static int GetNext(int _id);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void SetSelectedChoice(int _id, int _selectedChoice);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static bool SetCurrentDialogueID(int _id);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static int GetCurrentDialogueID();
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void ChangeGameState(string _name);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static bool EntityIsActive(string _entityName, string _sceneName);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void EntityActivate(string _entityName, string _sceneName);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void EntityDeactivate(string _entityName, string _sceneName);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static string EntityGetParent(string _entityName, string _sceneName);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static int EntityGetParentId(string _entityName, string _sceneName);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static string GetCurrentGameStateName();
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void PauseScene(string _name);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void PlayScene(string _name);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void GetCurrentCameraScaleX();
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void GetCurrentCameraScaleY();
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void GetCurrentCameraPosX();
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void GetCurrentCameraPosY();
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void SetCurrentCameraScaleX(float _x);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void SetCurrentCameraScaleY(float _y);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void SetCurrentCameraPosX(float _x);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void SetCurrentCameraPosY(float _y);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void SetCurrentImageIndex(string _entityName, string _sceneName, int _index);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static int GetCurrentImageIndex(string _entityName, string _sceneName);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void SetTexture(string _entityName, string _sceneName, string _path);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static string GetTexture(string _entityName, string _sceneName);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static bool EntitiesCollided(string _entityName1, string _entityName2, string _sceneName);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static bool CheckCollision(string _entityName1, string _entityName2, string _sceneName, bool _dynamicCheck);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void GameStateExit();
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static double GetDeltaTime();
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void PlayAnySound(string _name, int _channel);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static bool IsPlaying(int _channel);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void PlayBGSound(string _name, int _channel);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static float GetPosX(string _entityName, string _sceneName);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static float GetPosY(string _entityName, string _sceneName);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void SetPosX(string _entityName, string _sceneName, float _posX);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void SetPosY(string _entityName, string _sceneName, float _posY);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static float GetScaleX(string _entityName, string _sceneName);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static float GetScaleY(string _entityName, string _sceneName);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void SetScaleX(string _entityName, string _sceneName, float _scaleX);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void SetScaleY(string _entityName, string _sceneName, float _scaleY);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static float GetRotate(string _entityName, string _sceneName);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal extern static void SetRotate(string _entityName, string _sceneName, float _rotate);
+	}
+}
---
+++ MemoryLeak/resources/SCRIPTING/TestMonoScript.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace BonVoyage
+{
+    public class TestMonoScript
+    {
+        private bool intro; // player start
+        private bool introdone;
+
+        private bool encounter1; // meet girl
+        private bool encounter1done;
+
+        private bool encounter2; // meet pasenger
+        private bool encounter2done;
+
+        private bool encounter3; // deliver passenger
+        private bool encounter3done;
+
+        private int clickcount;
+
+        private string hello;
+
+        public void Init()
+        {
+            intro = true;
+            introdone = false;
+
+            encounter1 = false;
+            encounter1done = false;
+
+            encounter2 = false;
+            encounter2done = false;
+
+            encounter3 = false;
+            encounter3done = false;
+
+            clickcount = 0;
+
+            // Loading dialogue file
+            InternalCalls.LoadDialogs("Dialogue LittleGirl 0");
+        }
+
+        public void Update()
+        {
+            //if (InternalCalls.EntitiesCollided("Boat", "LittleGirlBox", "Level1")) { encounter1 = true; }
+            //if (InternalCalls.EntitiesCollided("Boat", "PassengerBox", "Level1")) { encounter2 = true; }
+            //if (InternalCalls.EntitiesCollided("PassengerBox", "PassengerDeliver", "Level1")) { encounter3 = true; }
+
+            if (intro && !introdone)
+            {
+
+                CameraZoomIn();
+
+                if (clickcount == 0)
+                {
+                    // [] show playertext
+                    InternalCalls.SetPosX("Boat", "Level1", 160.0f); // freeze boat in place at spawn // error
+                    InternalCalls.SetPosY("Boat", "Level1", 120.0f); // freeze boat in place at spawn // error
+                    ClickCheck();
+                }
+
+                if (clickcount == 1)
+                {
+                    // [] hide playertext
+                    // InternalCalls.EntityDeactivate("testbox", "Level1"); // error
+                    introdone = true;
+                }
+            }
+
+            else if (encounter1 && !encounter1done)
+            {
+
+                CameraZoomIn();
+
+                if (clickcount == 1)
+                {
+                    // [] show littlegirltext
+                    ClickCheck();
+                }
+
+                if (clickcount == 2)
+                {
+                    // [] change littlegirltext
+                    // [] show playertext
+                    ClickCheck();
+                }
+
+                if (clickcount == 3)
+                {
+                    // [] hide playertext
+                    // [] change littlegirltext
+                    ClickCheck();
+                }
+
+                if (clickcount == 4)
+                {
+                    // hide littlegiirltext
+                    encounter1done = true;
+                }
+            }
+
+            else if (encounter2 && !encounter2done)
+            {
+
+                CameraZoomIn();
+
+                if (clickcount == 4)
+                {
+                    // [] show passengertext
+                    ClickCheck();
+                }
+
+                if (clickcount == 5)
+                {
+                    // [] change passengertext
+                    // [] show playertext
+                    ClickCheck();
+                }
+
+                if (clickcount == 6)
+                {
+                    // [] hide playertext
+                    // [] change passengeretext
+                    ClickCheck();
+                }
+
+                if (clickcount == 7)
+                {
+                    // hide passengertext
+                    encounter1done = true;
+                }
+            }
+
+            else if (encounter3 && !encounter3done)
+            {
+                // [] show passengertext
+                // game over
+            }
+
+            else
+            {
+                CameraZoomOut();
+            }
+
+            if (InternalCalls.CheckKeyPress(68))
+            {
+                this.hello = "PRESSED!";
+                Console.WriteLine("Printing from C#: " + this.hello);
+
+                // Printing dialogue index 1 of loaded dialogue file
+                Console.WriteLine(InternalCalls.GetDialogue(1));
+            }
+        }
+
+        public void Exit()
+        {
+            Console.WriteLine("Exiting...");
+        }
+
+        public void ClickCheck()
+        {
+            if (InternalCalls.CheckKeyPress(349)) { clickcount++; } //left click
+        }
+
+        public void CameraZoomIn()
+        {
+            InternalCalls.SetCurrentCameraScaleX(700);
+        }
+
+        public void CameraZoomOut()
+        {
+            InternalCalls.SetCurrentCameraScaleX(1600);
+        }
+    }
+}
