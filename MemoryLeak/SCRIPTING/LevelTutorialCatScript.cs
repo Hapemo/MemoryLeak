02a03489756cf2389ff7f882fdefa38936f775d8
S13nd3rT3l3tub <40754433+S13nd3rT3l3tub@users.noreply.github.com>
2023-02-24 05:48:56 +0800

    Pushing my tested changes for continuation

---
+++ MemoryLeak/SCRIPTING/LevelTutorialCatScript.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace BonVoyage {
+
+  public class LevelTutorialCatScript : BaseScript {
+    private int colliderBox;
+    private int playerBoat;
+
+    public void Alive(int _ENTITY) {
+      THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+    }
+
+    public void Init(int _ENTITY) {
+      colliderBox = VI.Entity.GetId("CatDialogueCollider", "LevelTutorial");
+      playerBoat = VI.Entity.GetId("Boat", "LevelTutorial");
+    }
+
+    public void Update(int _ENTITY) {
+      if (VI.Physics.EntitiesCollided(colliderBox, playerBoat)) {
+        
+        Console.WriteLine("collided");
+        if (!LevelTutorialDialogueScript.runCatDialog)
+            LevelTutorialDialogueScript.runCatDialog = true;
+      }
+    }
+
+    public void FixedUpdate(int _ENTITY) {
+
+    }
+
+    public void Exit(int _ENTITY) {
+
+    }
+
+    public void Dead(int _ENTITY) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
