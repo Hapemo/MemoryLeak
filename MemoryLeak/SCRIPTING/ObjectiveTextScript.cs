23bafc5ea8b7c81207670146e377b186197ebee5
Hapemo <jazz.teohyj@outlook.com>
2023-02-24 00:14:36 +0800

    midway scripting for girl dialog

---
+++ MemoryLeak/SCRIPTING/ObjectiveTextScript.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace BonVoyage {
+  public class ObjectiveTextScript : BaseScript {
+
+    static private bool showingObjective;
+    static private string objective;
+    private const string toggleText = "Objective: Click to view";
+
+    public void Alive(int _ENTITY) {
+      THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+    }
+
+    public void Init(int _ENTITY) {
+      showingObjective = false;
+    }
+
+    public void Update(int _ENTITY) {
+      if (THIS.Input.Button.Clicked()) {
+        showingObjective = !showingObjective;
+
+        if (showingObjective) THIS.Text.Update(objective);
+        else THIS.Text.Update(toggleText);
+      }
+
+    }
+
+    public void FixedUpdate(int _ENTITY) {
+
+    }
+
+    public void Exit(int _ENTITY) {
+
+    }
+
+    public void Dead(int _ENTITY) {
+
+    }
+
+    static public void UpdateText(string str) {
+      if (showingObjective && objective != str) THIS.Text.Update(str);
+      objective = str;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
