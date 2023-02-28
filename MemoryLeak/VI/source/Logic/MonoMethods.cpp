b55c9a97dc03cb4deb30c282d8bc2d920776dabb
jiawendh <jiawendh@gmail.com>
2023-03-01 04:30:25 +0800

    internal calls

--- MemoryLeak/VI/source/Logic/MonoMethods.cpp
+++ MemoryLeak/VI/source/Logic/MonoMethods.cpp
@@ -568,17 +568,17 @@ void MonoMethods::RegisterCalls() {
 	mono_add_internal_call("VI.Animation/FrameCount::Set", &MM::iAnimation::SetFrameCount);
 	mono_add_internal_call("VI.Animation/FrameCount::Get", &MM::iAnimation::GetFrameCount);
 
-	mono_add_internal_call("VI.Animation/Move::Start", &VI::iAnimation::iTransform::Start);
-	mono_add_internal_call("VI.Animation/Move::SetNext", &VI::iAnimation::iTransform::SetNext);
-	mono_add_internal_call("VI.Animation/Move::Stop", &VI::iAnimation::iTransform::Stop);
-	mono_add_internal_call("VI.Animation/Move::StopAfterEndLoop", &VI::iAnimation::iTransform::StopAfterEndLoop);
-	mono_add_internal_call("VI.Animation/Move::ReverseOrder", &VI::iAnimation::iTransform::ReverseOrder);
-	mono_add_internal_call("VI.Animation/Move::SetLoopCycle", &VI::iAnimation::iTransform::SetLoopCycle);
-	mono_add_internal_call("VI.Animation/Move::AddTransform", &VI::iAnimation::iTransform::AddTransform);
-	mono_add_internal_call("VI.Animation/Move::AddTransformDifference", &VI::iAnimation::iTransform::AddTransformDifference);
-	mono_add_internal_call("VI.Animation/Move::SetCalculatedTimeFromPosition", &VI::iAnimation::iTransform::SetCalculatedTimeFromPosition);
-	mono_add_internal_call("VI.Animation/Move::SetCalculatedTimeFromRotation", &VI::iAnimation::iTransform::SetCalculatedTimeFromRotation);
-	mono_add_internal_call("VI.Animation/Move::SetCalculatedTimeFromScale", &VI::iAnimation::iTransform::SetCalculatedTimeFromScale);
+	mono_add_internal_call("VI.Animation/Transform::Start", &VI::iAnimation::iTransform::Start);
+	mono_add_internal_call("VI.Animation/Transform::SetNext", &VI::iAnimation::iTransform::SetNext);
+	mono_add_internal_call("VI.Animation/Transform::Stop", &VI::iAnimation::iTransform::Stop);
+	mono_add_internal_call("VI.Animation/Transform::StopAfterEndLoop", &VI::iAnimation::iTransform::StopAfterEndLoop);
+	mono_add_internal_call("VI.Animation/Transform::ReverseOrder", &VI::iAnimation::iTransform::ReverseOrder);
+	mono_add_internal_call("VI.Animation/Transform::SetLoopCycle", &VI::iAnimation::iTransform::SetLoopCycle);
+	mono_add_internal_call("VI.Animation/Transform::AddTransform", &VI::iAnimation::iTransform::AddTransform);
+	mono_add_internal_call("VI.Animation/Transform::AddTransformDifference", &VI::iAnimation::iTransform::AddTransformDifference);
+	mono_add_internal_call("VI.Animation/Transform::SetCalculatedTimeFromPosition", &VI::iAnimation::iTransform::SetCalculatedTimeFromPosition);
+	mono_add_internal_call("VI.Animation/Transform::SetCalculatedTimeFromRotation", &VI::iAnimation::iTransform::SetCalculatedTimeFromRotation);
+	mono_add_internal_call("VI.Animation/Transform::SetCalculatedTimeFromScale", &VI::iAnimation::iTransform::SetCalculatedTimeFromScale);
 
 	// Particle System
 	mono_add_internal_call("VI.ParticleSystem::GetDensity", &VI::iParticleSystem::GetDensity);
