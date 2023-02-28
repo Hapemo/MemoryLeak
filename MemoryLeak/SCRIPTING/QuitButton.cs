1fd565a1fb56c2ce1a33de6483963d598d1e7066
kewyj <74573283+kewyj@users.noreply.github.com>
2023-03-01 05:25:49 +0800

    p

--- MemoryLeak/SCRIPTING/QuitButton.cs
+++ MemoryLeak/SCRIPTING/QuitButton.cs
@@ -2,14 +2,14 @@
 using System.Runtime.CompilerServices;
 
 namespace BonVoyage {
-    public class PlayButton : BaseScript
+    public class QuitButton : BaseScript
     {
         public void Alive(int _ENTITY) {
             THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
         }
 
         public void Init(int _ENTITY) {
-            
+
         }
 
         public void EarlyUpdate(int _ENTITY) {
@@ -17,9 +17,16 @@ namespace BonVoyage {
         }
 
         public void Update(int _ENTITY) {
-            THIS.StoreId(_ENTITY);
+            THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
             if (THIS.Input.Button.Released())
-                VI.GameState.Go("CutScene");
+            {
+                VI.Scene.Pause("How_To_Play");
+                VI.Scene.Pause("Menu_Main");
+                VI.Scene.Pause("Settings");
+                VI.Scene.Pause("Credits");
+                VI.Scene.Pause("TransitionScene");
+                VI.Scene.Play("Quit Confirmation");
+            }
         }
 
         public void FixedUpdate(int _ENTITY) {
