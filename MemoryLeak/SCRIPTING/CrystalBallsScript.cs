106c80358665f488395b1f6bdf0af6f34b697db9
Huang Wei Jhin <wjhuang15@gmail.com>
2023-03-01 00:32:24 +0800

    say want game jam but go different level 1#leadbyexample

---
+++ MemoryLeak/SCRIPTING/CrystalBallsScript.cs
@@ -0,0 +1,106 @@
+﻿/*!*****************************************************************************
+\file CrystalBallScript.cs
+\author Huang Wei Jhin
+\par DP email: h.weijhin\@digipen.edu
+\par Group: Memory Leak Studios
+\date 28-01-2023
+\brief
+CrystalBallScript contains multiple script components to run the Level1 of the game.
+Part of which are:
+- Toggling of the mini maps
+    -Minimap
+    -WeatherMap
+    -EmemyMap
+*******************************************************************************/
+using System;
+using System.Runtime.CompilerServices;
+
+namespace BonVoyage {
+    public class CrystalBallScript
+    {
+        int toggle = 0; // 0 minimap, 1, weathermap, 2 enemymap
+        int prevTog = -1;
+        static bool big = false;
+        static bool active = true;
+
+        public void Alive(int _ENTITY)
+        {
+            THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+        }
+
+        public void Init(int _ENTITY) {
+            
+            VI.Entity.s_Deactivate("crystalBall", "CrystalBalls");
+        }
+
+        public void Update(int _ENTITY)
+        {
+            VI.Entity.s_Activate("crystalBall", "CrystalBalls");
+            if ((VI.Input.Button.s_Released("toggleMap", "CrystalBalls")) == true)
+            {
+                toggle = toggle >= 2 ? 0 : (toggle + 1);
+            }
+            if(prevTog != toggle)
+            { 
+                if (toggle == 0)
+                {
+                    VI.Entity.s_Deactivate("weathermap", "WeatherMap");
+                    VI.Entity.s_Deactivate("enemymap", "EnemyMap");
+                    VI.Scene.Play("MiniMap");
+                    VI.Entity.s_Activate("minimap", "MiniMap");
+                    VI.Scene.Pause("WeatherMap");
+                    VI.Scene.Pause("EnemyMap");
+                    
+                }
+                else if (toggle == 1)
+                {
+                    VI.Entity.s_Deactivate("minimap", "MiniMap");
+                    VI.Entity.s_Deactivate("enemymap", "EnemyMap");
+                    VI.Scene.Pause("MiniMap");
+                    VI.Scene.Play("WeatherMap");
+                    VI.Entity.s_Activate("weathermap", "WeatherMap");
+                    VI.Scene.Pause("EnemyMap");
+                    
+
+                }
+                else if (toggle == 2)
+                {
+                    VI.Entity.s_Deactivate("minimap", "MiniMap");
+                    VI.Entity.s_Deactivate("weathermap", "WeatherMap");
+                    VI.Scene.Pause("MiniMap");
+                    VI.Scene.Pause("WeatherMap");
+                    VI.Scene.Play("EnemyMap");
+                    VI.Entity.s_Activate("enemymap", "EnemyMap");
+                    
+                }
+                else //if (toggle == 3)
+                {
+                    VI.Entity.s_Deactivate("minimap", "MiniMap");
+                    VI.Entity.s_Deactivate("enemymap", "EnemyMap");
+                    VI.Entity.s_Deactivate("weathermap", "WeatherMap");
+                    VI.Scene.Pause("MiniMap");
+                    VI.Scene.Pause("WeatherMap");
+                    VI.Scene.Pause("EnemyMap");
+                    
+                }
+                prevTog = toggle;
+            }
+
+        }
+        public void Exit(int _ENTITY) {
+            toggle=0;
+            prevTog = -1;
+            VI.Entity.s_Deactivate("minimap", "MiniMap");
+            VI.Entity.s_Deactivate("enemymap", "EnemyMap");
+            VI.Entity.s_Deactivate("weathermap", "WeatherMap");
+            VI.Entity.s_Deactivate("crystalBall", "CrystalBalls");
+            VI.Scene.Pause("MiniMap");
+            VI.Scene.Pause("WeatherMap");
+            VI.Scene.Pause("EnemyMap");
+        }
+        public void Dead(int _ENTITY)
+        {
+
+        }
+    }
+}
--- MemoryLeak/SCRIPTING/MiniMapScript.cs
+++ MemoryLeak/SCRIPTING/MiniMapScript.cs
@@ -16,60 +16,39 @@ using System.Runtime.CompilerServices;
 namespace BonVoyage {
     public class MiniMapScript
     {
-        static bool init = true;
-
-        public void Alive(int _ENTITY) {
+        static bool big = false;
+        public void Alive(int _ENTITY)
+        {
             THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
-            VI.Entity.s_SetActive("minimapbig", "WeatherMap", false);
-            VI.Entity.s_SetActive("minimap", "WeatherMap", false);
-            VI.Entity.s_SetActive("minimaphead", "WeatherMap", false);
         }
-
         public void Init(int _ENTITY) {
-            VI.Entity.s_SetActive("minimap", "WeatherMap", true);
-            VI.Entity.s_SetActive("minimapbig", "WeatherMap", true);
-            VI.Entity.s_SetActive("minimaphead", "WeatherMap", true);
 
+            VI.Entity.s_SetActive("minimapbig", "MiniMap", false);
         }
-
-        public void EarlyUpdate(int _ENTITY) { }
 
         public void Update(int _ENTITY) {
-            if (init)
-            {
-                init = false;
-                
-            }
             //////////////////////////////////////////////init
-            VI.Entity.s_SetActive("minimaphead", "WeatherMap", true);
-            VI.Entity.s_SetActive("minimap", "WeatherMap", true);
-
-            ///
-            if (CrystalBallScript.Big())
+            if ((VI.Input.Button.s_Released("minimap", "MiniMap")) == true)
             {
-                VI.Entity.s_SetActive("minimapbig", "WeatherMap", true);
-            }
-            else
-            {
-                VI.Entity.s_SetActive("minimapbig", "WeatherMap", false);
+                big = !big;
+                if (big)
+                {
+                    VI.Entity.s_SetActive("minimapbig", "MiniMap", true);
+                }
+                else
+                {
+                    VI.Entity.s_SetActive("minimapbig", "MiniMap", false);
+                }
             }
 
         }
-
-        public void FixedUpdate(int _ENTITY) {
-
-        }
-
-        public void LateUpdate(int _ENTITY) { }
 
         public void Exit(int _ENTITY) {
-            VI.Entity.s_SetActive("minimap", "WeatherMap", false);
-            VI.Entity.s_SetActive("minimapbig", "WeatherMap", false);
-            VI.Entity.s_SetActive("minimaphead", "WeatherMap", false);
-
+            big = false;
+            VI.Entity.s_SetActive("minimapbig", "MiniMap", false);
         }
-
-        public void Dead(int _ENTITY) {
+        public void Dead(int _ENTITY)
+        {
 
         }
     }
