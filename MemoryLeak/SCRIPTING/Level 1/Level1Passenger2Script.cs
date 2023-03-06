24ea709fc47f891b52e63c2006c0521fca098529
S13nd3rT3l3tub <40754433+S13nd3rT3l3tub@users.noreply.github.com>
2023-03-06 22:21:43 +0800

    Edits to variables

--- MemoryLeak/SCRIPTING/Level 1/Level1Passenger2Script.cs
+++ MemoryLeak/SCRIPTING/Level 1/Level1Passenger2Script.cs
@@ -11,11 +11,11 @@ namespace BonVoyage {
             // Get required entities
             playerBoat = VI.Entity.GetId("Boat", "Level1");
             triggerBox = VI.Entity.GetId("Passenger2Box", "Level1");
-            destinationA_Box = VI.Entity.GetId("DoubleStoryHouseDropOffPoint", "Level1");
-            destinationA_RenderLocation = VI.Entity.GetId("DoubleStoryHouseDestRender", "Level1");
+            correctDestination_Box = VI.Entity.GetId("DoubleStoryHouseDropOffPoint", "Level1");
+            correctDestination_RenderLocation = VI.Entity.GetId("DoubleStoryHouseDestRender", "Level1");
 
-            destinationB_Box = VI.Entity.GetId("PortHouseDropOffPoint", "Level1");
-            destinationB_RenderLocation = VI.Entity.GetId("PortHouseDestRender", "Level1");
+            wrongDestination_Box = VI.Entity.GetId("PortHouseDropOffPoint", "Level1");
+            wrongDestination_RenderLocation = VI.Entity.GetId("PortHouseDestRender", "Level1");
 
             // Store original scale x value
             InitialScaleX = VI.Transform.Scale.GetX(_ENTITY);
@@ -70,18 +70,30 @@ namespace BonVoyage {
             }
 
             // Check if passenger reaches destination
-            if (VI.Physics.CheckCollision(destinationA_Box, _ENTITY, false)) {
+            if (VI.Physics.IsCollided(correctDestination_Box, _ENTITY)) {
                 // Move on to detaching animation
                 AttachedToPlayer = false;
                 DetachFromPlayerAnimation = true;
                 PlayerScript.PlayerInOtherAnimation = true;
+
+                // Set flag
+                correctDestinationDelivery = true;
+            }
+            else if (VI.Physics.IsCollided(wrongDestination_Box, _ENTITY)) {
+                // Move on to detaching animation
+                AttachedToPlayer = false;
+                DetachFromPlayerAnimation = true;
+                PlayerScript.PlayerInOtherAnimation = true;
+
+                // Set flag
+                correctDestinationDelivery = false;
             }
 
             // Play animation of passenger detaching from player
             if (DetachFromPlayerAnimation) {
                 // Animate detachment to player
                 // returns true once complete
-                if (DetachPassengerFromPlayer(_ENTITY, destinationA_RenderLocation, InitialScaleX)) {
+                if (DetachPassengerFromPlayer(_ENTITY, correctDestination_RenderLocation, InitialScaleX)) {
                     // Animation complete
                     DetachFromPlayerAnimation = false;
                     PlayerScript.PlayerInOtherAnimation = false;
@@ -90,11 +102,30 @@ namespace BonVoyage {
 
             }
         
+            // Passenger was delivered
             if (DestinationReached) {
+                // Return layer value
                 VI.Texture.SetLayer(_ENTITY, InitialLayerVal);
 
+
+
                 // Restore passenger's original scale value
+                // Returns true when completed
                 if (RestorePassengerScale(_ENTITY, InitialScaleX)){
+                    // Run logic based on whether destination delievered was correct
+                    switch (correctDestinationDelivery) {
+                        case true:
+                            // Run second script/logic from here
+                            break;
+                        case false:
+                            // Run second script/logic from here
+                            break;
+                        default:
+                            // Do nothing
+                            break;
+                    }
+
+                    // Set flag to get out
                     DestinationReached = false;
                 }
             }
