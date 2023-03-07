68ab4d7d0f92f5b980e55559e1ef3ce6cdb97a48
Huang Wei Jhin <wjhuang15@gmail.com>
2023-03-08 00:07:28 +0800

    idk

---
+++ MemoryLeak/SCRIPTING/ObjectiveText.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace BonVoyage {
+  public class ObjectiveText : BaseScript {
+    public void Alive(int _ENTITY) {
+      THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+    }
+
+    public void Init(int _ENTITY) {
+      THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+
+    }
+
+    public void EarlyUpdate(int _ENTITY) {
+      THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+
+    }
+
+    public void Update(int _ENTITY) {
+      THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+
+    }
+
+    public void FixedUpdate(int _ENTITY) {
+      THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+
+    }
+
+    public void LateUpdate(int _ENTITY) {
+      THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+
+    }
+
+    public void Exit(int _ENTITY) {
+      THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+
+    }
+
+    public void Dead(int _ENTITY) {
+      THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+
+    }
+  }
+}
