a4d1a3eee50d46ea40ed5d233457d448737d94b4
Hapemo <j.teoh@digipen.edu>
2023-03-09 20:53:03 +0800

    midway through level2 transition

---
+++ MemoryLeak/SCRIPTING/Level 1/SwitchToLevel2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace BonVoyage {
+    public class SwitchToLevel2 : BaseScript
+    {
+        private int playerBoat;
+        private bool ranOnceInit = true;
+
+        public void Alive(int _ENTITY) {
+            THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+
+            playerBoat = VI.Entity.GetId("Boat", "Level1");
+        }
+
+        public void Init(int _ENTITY) {
+            THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+
+            playerBoat = VI.Entity.GetId("Boat", "Level1");
+        }
+
+        public void EarlyUpdate(int _ENTITY) {
+            THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+        
+        }
+
+        public void Update(int _ENTITY) {
+            // Temporary implementation TODO change this
+            if (ranOnceInit) {
+                ranOnceInit = false;
+                
+                playerBoat = VI.Entity.GetId("Boat", "Level1");
+            }
+
+            THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+
+            if (VI.Physics.IsCollided(_ENTITY, playerBoat)&& Level1DialogManager.passengerDialogProgress >= 20) {
+                TransitionSquare.FadeOut("Level2");
+                //VI.GameState.Go("Level1");
+            }
+        }
+
+        public void FixedUpdate(int _ENTITY) {
+            THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+
+        }
+
+        public void LateUpdate(int _ENTITY) {
+            THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+        
+        }
+
+        public void Exit(int _ENTITY) {
+            THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+
+        }
+
+        public void Dead(int _ENTITY) {
+            THIS.StoreId(_ENTITY); // DO NOT REMOVE!!!
+
+        }
+    }
+}
